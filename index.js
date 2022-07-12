d3945cd552fcf3bc175437dd8d5e78696e300181
Spencer Dunlap <spencerdunlap@me.com>
2022-07-11 21:46:38 -0500

    SPA

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { Header, Nav, Main, Footer } from "/components";
+import * as state from "/store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo(window.location.origin);
+
+function render(st = state.Home) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(st)}
+  ${Nav(state.Links)}
+  ${Main(st)}
+  ${Footer()}
+  `;
+  router.updatePageLinks();
+  addEventListeners(st);
+}
+render(state.home);
+
+function addEventListeners(st) {
+  // add event listeners to Nav items for navigation
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
+      event.preventDefault();
+      render(state[event.target.title]);
+    })
+  );
+
+  // add menu toggle to bars icon in nav bar
+  // document.querySelector(".fa-bars").addEventListener("click", () => {
+  //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+}
---
+++ store/Links.js
@@ -0,0 +1,6 @@
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Bio", text: "Bio" },
+  { title: "Order", text: "Order" },
+  { title: "Pizza", text: "Pizza" }
+];
